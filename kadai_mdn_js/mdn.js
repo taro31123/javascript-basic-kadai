b0b3c1fd2749795c96af78d58693214f61a38049
taro31123 <natom779work@gmail.com>
2024-10-18 00:02:24 +0900

    add課題MDNwo

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,14 @@
+// todayを定義して日付取得
+const today = new Date();
+
+//取得した日付の出力1
+console.log(
+  `${today.getFullYear()}年${today.getMonth()}月${today.getDate()}日`
+);
+
+//取得した日付の出力2
+// console.log(
+//   today.getFullYear() + "年",
+//   today.getMonth() + "月",
+//   today.getDate() + "日"
+// );
