d670d4a2f957a1db9806b4da594cf3a64484714e
taro31123 <nztom779work@gmail.com>
2024-10-18 22:43:52 +0900

    Fix mdn.jsを修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,14 +1,7 @@
 // todayを定義して日付取得
 const today = new Date();
 
-//取得した日付の出力1
+//取得した日付の出力
 console.log(
   `${today.getFullYear()}年${today.getMonth()}月${today.getDate()}日`
 );
-
-//取得した日付の出力2
-// console.log(
-//   today.getFullYear() + "年",
-//   today.getMonth() + "月",
-//   today.getDate() + "日"
-// );
