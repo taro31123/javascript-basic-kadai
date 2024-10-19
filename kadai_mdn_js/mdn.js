681234faf11192f6f60b94f81b82bd3bd0f7c164
taro31123 <nztom779work@gmail.com>
2024-10-19 10:22:23 +0900

    fix 月が正しく表示されるように修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -3,5 +3,5 @@ const today = new Date();
 
 //取得した日付の出力
 console.log(
-  `${today.getFullYear()}年${today.getMonth()}月${today.getDate()}日`
+  `${today.getFullYear()}年${today.getMonth() + 1}月${today.getDate()}日`
 );
